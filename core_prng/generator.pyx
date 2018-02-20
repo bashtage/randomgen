8081fa9941c2a9846d11f798b13e29a82d95e9ec
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2018-02-20 16:29:07 +0000

    CLN: Remove unneeded code
    Remove unnecessary code and add docstrings

--- core_prng/generator.pyx
+++ core_prng/generator.pyx
@@ -1,7 +1,6 @@
 import numpy as np
 cimport numpy as np
-from libc.stdint cimport uint64_t
-from cpython.pycapsule cimport PyCapsule_New, PyCapsule_IsValid, PyCapsule_GetPointer
+from cpython.pycapsule cimport PyCapsule_IsValid, PyCapsule_GetPointer
 from common cimport *
 
 from core_prng.core_prng import CorePRNG
@@ -9,6 +8,20 @@ from core_prng.core_prng import CorePRNG
 np.import_array()
 
 cdef class RandomGenerator:
+    """
+    Prototype Random Generator that consumes randoms from a CorePRNG class
+
+    Parameters
+    ----------
+    prng : CorePRNG, optional
+        Object exposing a PyCapsule containing state and function pointers
+
+    Examples
+    --------
+    >>> from core_prng.generator import RandomGenerator
+    >>> rg = RandomGenerator()
+    >>> rg.random_integer()
+    """
     cdef public object __core_prng
     cdef anon_func_state anon_rng_func_state
 
