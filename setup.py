27bd211682775964e6ef87f7d3191a24fe8a88dc
David Ham <david.ham@imperial.ac.uk>
2019-04-02 11:08:24 +0100

    Set the correct input encoding

--- setup.py
+++ setup.py
@@ -340,7 +340,7 @@ setup(
     author='Kevin Sheppard',
     author_email='kevin.k.sheppard@gmail.com',
     distclass=BinaryDistribution,
-    long_description=open('README.rst').read(),
+    long_description=open('README.rst', encoding="utf-8").read(),
     description='Random generator supporting multiple PRNGs',
     url='https://github.com/bashtage/randomgen',
     keywords=['pseudo random numbers', 'PRNG', 'RNG', 'RandomState', 'random',
