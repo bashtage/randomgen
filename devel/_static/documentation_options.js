79c95c42b8220e5509e0540bdaff26ec56a9a9e0
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-04-02 14:06:28 +0000

    Update docs after building Travis build 404 of bashtage/randomgen
    The docs were built from the tag 'v1.16.2' against the commit
    e4af9eb629814062a0c2ea4c00a715a5bc54841e.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/bashtage/randomgen/jobs/514695761.
    
    The doctr command that was run is
    
        /home/travis/miniconda3/envs/randomgen-test/bin/doctr deploy devel --build-tags

--- devel/_static/documentation_options.js
+++ devel/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v1.16.2+1.ge4af9eb',
+    VERSION: 'v1.16.2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
