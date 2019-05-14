b3c966785bf6b3059ea2bae7eb0e3ea81374c61e
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2019-05-14 17:20:07 +0100

    TST: Test more of the deprecations
    Ensure deprecations are raised, and fix any missed

--- randomgen/tests/test_final_release_changes.py
+++ randomgen/tests/test_final_release_changes.py
@@ -1,8 +1,24 @@
 import pytest
 
-from randomgen import Generator
+from randomgen import (DSFMT, MT19937, PCG32, PCG64, Philox, Generator,
+                       ThreeFry, ThreeFry32, Xoroshiro128, Xorshift1024,
+                       Xoshiro256StarStar, Xoshiro512StarStar)
 
 random_gen = Generator()
+
+bit_generators = [DSFMT, MT19937, PCG32, PCG64, Philox,
+                  ThreeFry, ThreeFry32, Xoroshiro128, Xorshift1024,
+                  Xoshiro256StarStar, Xoshiro512StarStar]
+
+
+@pytest.fixture('module', params=bit_generators)
+def bit_generator(request):
+    return request.param
+
+
+@pytest.fixture('module', params=[True, False])
+def endpoint(request):
+    return request.param
 
 
 def test_random_sample_deprecated():
@@ -13,3 +29,43 @@ def test_random_sample_deprecated():
 def test_randint_deprecated():
     with pytest.deprecated_call():
         random_gen.randint(10)
+
+
+def test_rand_deprecated():
+    with pytest.deprecated_call():
+        random_gen.rand(10)
+
+
+def test_randn_deprecated():
+    with pytest.deprecated_call():
+        random_gen.randn(10)
+
+
+def test_brng_deprecated():
+    with pytest.deprecated_call():
+        random_gen.brng
+
+
+def test_generator_raises(bit_generator):
+    bg = bit_generator()
+    with pytest.raises(NotImplementedError):
+        bg.generator
+
+
+def test_integers_closed():
+    with pytest.deprecated_call():
+        random_gen.integers(0, 10, closed=True)
+    with pytest.deprecated_call():
+        random_gen.integers(0, 10, closed=False)
+
+
+def test_integers_use_masked(endpoint):
+    with pytest.deprecated_call():
+        random_gen.integers(0, 10, use_masked=endpoint)
+
+
+def test_integers_large_negative_value():
+    with pytest.raises(ValueError):
+        random_gen.integers(0, -(2 ** 65), endpoint=endpoint)
+    with pytest.raises(ValueError):
+        random_gen.integers(0, [-(2 ** 65)], endpoint=endpoint)
