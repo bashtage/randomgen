3f06e9515bd17a48d1e5eda5ccfd3825d7e94730
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-06-04 19:10:50 +0000

    Update docs after building Travis build 843 of bashtage/randomgen
    The docs were built from the branch 'master' against the commit
    b552fdd4303f4af797e617a533ca11c2f9c22c6f.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/bashtage/randomgen/jobs/541387399.
    
    The doctr command that was run is
    
        /home/travis/miniconda3/envs/randomgen-test/bin/doctr deploy devel --build-tags

--- devel/_static/documentation_options.js
+++ devel/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v1.17.0.dev0+72.g3640cb3',
+    VERSION: 'v1.17.0.dev0+74.gb552fdd',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
