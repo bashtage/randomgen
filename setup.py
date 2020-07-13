45eb65e8bfc09ca43079c3fdbdc63cb03cbee500
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2020-07-13 16:08:58 +0100

    MAINT: Make clear 3.6 is minimum version supported

--- setup.py
+++ setup.py
@@ -319,10 +319,9 @@ classifiers = [
     "Operating System :: Unix",
     "Programming Language :: C",
     "Programming Language :: Cython",
-    "Programming Language :: Python :: 2.7",
-    "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
     "Topic :: Adaptive Technologies",
     "Topic :: Artistic Software",
     "Topic :: Office/Business :: Financial",
@@ -384,4 +383,5 @@ setup(
     ],
     zip_safe=False,
     install_requires=required,
+    python_requires='>=3',
 )
