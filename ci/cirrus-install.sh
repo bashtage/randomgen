836f65086b95e730a9159ae497d65b65aad38f51
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2022-05-17 11:56:06 +0100

    MAINT: Update cirrus

--- ci/cirrus-install.sh
+++ ci/cirrus-install.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-freebsd-update fetch
-freebsd-update install
-
 pkg install -y python39 py39-pip py39-numpy py39-cython py39-pytest
 python3.9 -m pip install -e . --no-build-isolation --user
 
