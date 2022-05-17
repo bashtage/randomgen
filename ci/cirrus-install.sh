dc56a052a0903e602fa59ab2b20154cfd75fe2bb
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2022-05-17 14:19:03 +0100

    MAINT: Update drone and cirrus

--- ci/cirrus-install.sh
+++ ci/cirrus-install.sh
@@ -5,5 +5,5 @@ pkg install -y python39  py39-numpy py39-cython wget
 python3.9 -m ensurepip --upgrade
 # wget https://bootstrap.pypa.io/get-pip.py
 # python get-pip.py
-python3.9 -m pip install pytest --user
+python3.9 -m pip install pytest wheel --user
 python3.9 -m pip install -e . --no-build-isolation --user
