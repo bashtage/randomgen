3b9512aa9f3676474cd37e6ddb17296e9a1f6759
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2022-05-17 12:28:19 +0100

    MAINT: Update cirrus

--- ci/cirrus-install.sh
+++ ci/cirrus-install.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-pkg install -y python39 py39-pip py39-numpy py39-cython py39-pytest
+
+pkg install -y python39  py39-numpy py39-cython wget
+python -m ensurepip --upgrade
+# wget https://bootstrap.pypa.io/get-pip.py
+# python get-pip.py
+python3.9 -m pip install pytest --user
 python3.9 -m pip install -e . --no-build-isolation --user
-
-
