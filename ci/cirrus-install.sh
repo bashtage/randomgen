5c61d9f6a70c9a3b8aad817c7a8f95b7891b6fec
Kevin Sheppard <kevin.sheppard@gmail.com>
2022-05-18 00:05:06 +0100

    MAINT: Improve cirrus

--- ci/cirrus-install.sh
+++ ci/cirrus-install.sh
@@ -5,7 +5,7 @@ pkg install -y python39  py39-numpy py39-cython wget git
 python3.9 -m ensurepip --upgrade
 # wget https://bootstrap.pypa.io/get-pip.py
 # python get-pip.py
-python3.9 -m pip install pytest wheel --user
+python3.9 -m pip install pytest wheel
 python3.9 -m pip wheel . -w wheelhouse/
 WHL=$(ls -t wheelhouse/randomgen-*.whl | head -n1)
-python3.9 -m pip install install ${WHL} --user
+python3.9 -m pip install install ${WHL}
