7176c032cf028cd42c5f7bc33e190ada983fbf08
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2022-05-18 08:57:52 +0100

    MAINT: Update CI

--- ci/cirrus-install.sh
+++ ci/cirrus-install.sh
@@ -5,7 +5,5 @@ pkg install -y python39  py39-numpy py39-cython wget git
 python3.9 -m ensurepip --upgrade
 python3.9 -m pip install wheel setuptools_scm[toml] pytest
 python3.9 -m pip list
+git fetch --tags
 python3.9 -m pip install -e . --no-build-isolation
-mkdir test-dir
-pushd test-dir
-echo $PWD
