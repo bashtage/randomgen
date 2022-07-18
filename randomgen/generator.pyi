f1fe2246fc7a6007d7a458a5acc676a76b799816
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2022-07-18 09:16:48 +0100

    MAINT: Improve typing and restore mypy defs

--- randomgen/generator.pyi
+++ randomgen/generator.pyi
@@ -11,6 +11,90 @@ class Generator:
     ...
     def __init__(self, bit_generator: Optional[BitGenerator] = ...) -> None: ...
 
+class ExtendedGenerator:
+    _bit_generator: BitGenerator
+    lock: Lock
+    _generator: Generator
+    def __init__(self, bit_generator: Optional[BitGenerator] = ...) -> None: ...
+    @property
+    def bit_generator(self) -> BitGenerator: ...
+    @property
+    def state(self) -> Dict[str, Any]: ...
+    @state.setter
+    def state(self, value: Dict[str, Any]) -> None: ...
+    @overload
+    def uintegers(self, size: None, bits: Literal[32, 64] = ...) -> int: ...
+    @overload
+    def uintegers(self, size: RequiredSize, bits: Literal[32, 64] = ...) -> ndarray: ...
+    @overload
+    def random(self) -> float: ...  # type: ignore[misc]
+    @overload
+    def random(self, size: None = ...) -> float: ...  # type: ignore[misc]
+    @overload
+    def random(
+        self, size: Size = ..., dtype: str = ..., out: Optional[ndarray] = ...
+    ) -> ndarray: ...
+    # Multivariate distributions:
+    def multivariate_normal(
+        self,
+        mean: ndarray,
+        cov: ndarray,
+        size: Size = ...,
+        check_valid: Literal["raise", "ignore", "warn"] = ...,
+        tol: float = ...,
+        *,
+        method: Literal["svd", "eigh", "cholesky", "factor"] = ...
+    ) -> ndarray: ...
+    @overload
+    def complex_normal(  # type: ignore[misc]
+        self,
+        loc: complex = ...,
+        gamma: complex = ...,
+        relation: complex = ...,
+    ) -> complex: ...
+    @overload
+    def complex_normal(  # type: ignore[misc]
+        self,
+        loc: complex = ...,
+        gamma: complex = ...,
+        relation: complex = ...,
+        size: RequiredSize = ...,
+    ) -> ndarray: ...
+    @overload
+    def complex_normal(
+        self,
+        loc: Union[complex, ndarray] = ...,
+        gamma: Union[complex, ndarray] = ...,
+        relation: Union[complex, ndarray] = ...,
+        size: Size = ...,
+    ) -> ndarray: ...
+    def standard_wishart(
+        self, df: int, dim: int, size: Size = ..., *, rescale: bool = ...
+    ) -> ndarray: ...
+    def wishart(
+        self,
+        df: Union[int, ndarray],
+        scale: ndarray,
+        size: Size = ...,
+        *,
+        check_valid: Literal["raise", "ignore", "warn"] = ...,
+        tol: float = ...,
+        rank: Optional[int] = ...,
+        method: Literal["svd", "eigh", "cholesky", "factor"] = ...
+    ) -> ndarray: ...
+    def multivariate_complex_normal(
+        self,
+        loc: ndarray,
+        gamma: Optional[ndarray] = ...,
+        relation: Optional[ndarray] = ...,
+        size: Size = ...,
+        *,
+        check_valid: Literal["raise", "ignore", "warn"] = ...,
+        tol: float = ...,
+        method: Literal["svd", "eigh", "cholesky", "factor"] = ...
+    ) -> ndarray: ...
+
+
 def _raises_not_implemented(*args: Any, **kwargs: Any) -> None: ...
 
 beta = _raises_not_implemented
