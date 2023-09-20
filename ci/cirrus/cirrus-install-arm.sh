2865c87c715bff3f84c05ddd2720cfdc1b85ed2f
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2023-09-20 12:04:03 +0100

    CI: Attempt to use ARM only

--- ci/cirrus/cirrus-install-arm.sh
+++ ci/cirrus/cirrus-install-arm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 apt-get update -y
-apt-get install build-essential -y
+apt-get install build-essential git -y
 python3 -m ensurepip --upgrade
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
