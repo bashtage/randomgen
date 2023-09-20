04f40752a93b3fcd3def1c2eb1e4b27a2271c3db
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2023-09-20 13:10:47 +0100

    CI: Figure out cirrus

--- ci/cirrus/cirrus-install-freebsd.sh
+++ ci/cirrus/cirrus-install-freebsd.sh
@@ -4,7 +4,7 @@
 pkg install -y python39  py39-numpy py39-cython wget git
 python3.9 -m ensurepip --upgrade
 python3.9 -m pip install --upgrade pip
-python3.9 -m pip install wheel "setuptools_scm[toml]>=7.1.0,<8.0.0" pytest packaging
+python3.9 -m pip install wheel "setuptools_scm[toml]>=7.1.0,<8.0.0" pytest pytest-xdist packaging
 python3.9 -m pip list
 git fetch --tags
 python3.9 -m pip install . --no-build-isolation -vv
