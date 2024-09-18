dfd63515db851af2e7eec644d57748aa1cbc60c3
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2024-09-18 13:27:21 +0100

    REF: Add test data

--- randomgen/src/pcg64/pcg64-test-data-gen.c
+++ randomgen/src/pcg64/pcg64-test-data-gen.c
@@ -20,9 +20,9 @@ int main() {
     uint64_t seed = 0ULL;
     // First 4 values produced by SeedSequence(0)
     __uint128_t seed_seq =
-      U128BIT_CONSTANT(15793235383387715774ULL, 12390638538380655177ULL);
+      PCG_128BIT_CONSTANT(15793235383387715774ULL, 12390638538380655177ULL);
     __uint128_t inc =
-      U128BIT_CONSTANT(2361836109651742017ULL, 3188717715514472916ULL);
+      PCG_128BIT_CONSTANT(2361836109651742017ULL, 3188717715514472916ULL);
     int i;
     uint64_t store[N];
     pcg64_srandom_r(&rng, seed_seq, inc);
@@ -51,9 +51,9 @@ int main() {
 
     seed = 0xDEADBEAFULL;
     // First values produced by SeedSequence(0xDEADBEAF)
-    seed = U128BIT_CONSTANT(5778446405158232650ULL, 4639759349701729399ULL);
-    inc = U128BIT_CONSTANT(13222832537653397986ULL, 2330059127936092250ULL);
-    pcg64_srandom_r(&rng, s, i);
+    seed_seq = PCG_128BIT_CONSTANT(5778446405158232650ULL, 4639759349701729399ULL);
+    inc = PCG_128BIT_CONSTANT(13222832537653397986ULL, 2330059127936092250ULL);
+    pcg64_srandom_r(&rng, seed_seq, inc);
     printf("0x%" PRIx64, (uint64_t)(rng.state >> 64));
     printf("%" PRIx64 "\n", (uint64_t)rng.state);
     printf("0x%" PRIx64, (uint64_t)(rng.inc >> 64));
