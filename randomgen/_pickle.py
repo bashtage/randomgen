c37ea024abe814b3145eaf86eb5c3237d38a0f30
Kevin Sheppard <bashtage@users.noreply.github.com>
2024-09-19 09:00:14 +0100

    BLD: Fix build requirements

--- randomgen/_pickle.py
+++ randomgen/_pickle.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import warnings
 
 from randomgen.aes import AESCounter
--- randomgen/tests/test_direct.py
+++ randomgen/tests/test_direct.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from functools import partial
 import os
 from os.path import join
