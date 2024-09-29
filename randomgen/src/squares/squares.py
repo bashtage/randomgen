b40c9b7289c854c57c14e1ff163306c98c944871
Kevin Sheppard <bashtage@users.noreply.github.com>
2024-09-29 15:48:45 +0100

    More work on squares

--- randomgen/src/squares/squares.py
+++ randomgen/src/squares/squares.py
@@ -1,5 +1,7 @@
 import numpy as np
 from numpy.random import SeedSequence
+
+
 class SequenceSampled:
     def __init__(self, seed_seq: SeedSequence, initial_draw=10):
         self.seed_seq = seed_seq
@@ -17,7 +19,9 @@ class SequenceSampled:
         print(self.index)
         if self.index > self.random_well.shape[0]:
             self.randoms_drawm *= 2
-            self.random_well = self.seed_seq.generate_state(self.randoms_drawm, dtype=np.uint64)
+            self.random_well = self.seed_seq.generate_state(
+                self.randoms_drawm, dtype=np.uint64
+            )
 
     def gen_bits(self, nbits):
         if self.bit_well_loc + nbits > 64:
@@ -56,8 +60,7 @@ class SequenceSampled:
         return self.gen_bits(4)
 
 
-
-#def generate_key(seed_seq:SeedSequence):
+# def generate_key(seed_seq:SeedSequence):
 seed_seq = SeedSequence(0)
 out = np.uint64(0)
 tmp = np.empty(14, dtype=np.uint8)
@@ -66,7 +69,7 @@ val = seed_seq.generate_state(1, np.uint64)[0]
 loc = val & 0x7
 val >>= np.uint64(3)
 # 2. Store in byte 0
-first = (2 * loc + 1)
+first = 2 * loc + 1
 out |= first
 # 3. Populate an array with bytes 1...15 ex a
 pos = 0
@@ -82,9 +85,9 @@ ss.shuffle(tmp)
 # 5. Store in Bytes 1...7 with first 7 locations form shuffled array
 last_value = 0
 for i in range(1, 8):
-    out |= np.uint64(tmp[i-1]) << np.uint64(4*i)
-    if i ==7:
-        last_value = tmp[i-1]
+    out |= np.uint64(tmp[i - 1]) << np.uint64(4 * i)
+    if i == 7:
+        last_value = tmp[i - 1]
 # 7. Choose a random number from 1,2,...,15 ex position 7
 # 8. Store in byte 8
 # 9. Populate bytes 9...15 with random bytes
@@ -95,7 +98,7 @@ for i in range(8, 16):
     while next_val == last_value or next_val == 0:
         next_val = ss.next_word()
     last_value = next_val
-    out |= np.uint64(next_val) << np.uint64(4*i)
+    out |= np.uint64(next_val) << np.uint64(4 * i)
 print(out)
 print(bin(out))
 print(hex(out))
