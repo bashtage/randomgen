44b674dc885a3a8333bbcbeb1018804aaf35f0a4
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2025-10-09 23:58:23 +0100

    BLD: Fix sse2

--- ci/cirrus/cirrus-install-arm.sh
+++ ci/cirrus/cirrus-install-arm.sh
@@ -4,8 +4,8 @@ apt-get update -y
 apt-get install build-essential git -y
 python3 -m ensurepip --upgrade
 python3 -m pip install --upgrade  --upgrade pip setuptools wheel pytest pytest-xdist packaging
-python3 -m pip install -r requirements.txt
-python3 -m pip install -r requirements-dev.txt
+python3 -m pip install -r requirements.txt -U
+python3 -m pip install -r requirements-dev.txt -U
 python3 -m pip list
 git fetch --tags
 python3 -m pip install . --no-build-isolation -vv
