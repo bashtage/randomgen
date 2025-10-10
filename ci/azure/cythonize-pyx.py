6ed43fc50c7ca499666c90f8d90c9aa36e410086
Kevin Sheppard <bashtage@users.noreply.github.com>
2025-10-10 09:43:11 +0100

    CI: add cythonization of C files

---
+++ ci/azure/cythonize-pyx.py
@@ -0,0 +1,45 @@
+import subprocess
+
+base = "cython --verbose -M --fast-fail -3 -X cpow=True -X boundscheck=False -X wraparound=False -X cdivision=True -X binding=True -X linetrace=True".split(
+    " "
+)
+
+pyx_files = [
+    "randomgen/aes.pyx",
+    "randomgen/broadcasting.pyx",
+    "randomgen/chacha.pyx",
+    "randomgen/common.pyx",
+    "randomgen/dsfmt.pyx",
+    "randomgen/efiix64.pyx",
+    "randomgen/entropy.pyx",
+    "randomgen/generator.pyx",
+    "randomgen/hc128.pyx",
+    "randomgen/jsf.pyx",
+    "randomgen/lxm.pyx",
+    "randomgen/mt19937.pyx",
+    "randomgen/mt64.pyx",
+    "randomgen/mtrand.pyx",
+    "randomgen/pcg32.pyx",
+    "randomgen/pcg64.pyx",
+    "randomgen/philox.pyx",
+    "randomgen/rdrand.pyx",
+    "randomgen/romu.pyx",
+    "randomgen/sfc.pyx",
+    "randomgen/sfmt.pyx",
+    "randomgen/speck128.pyx",
+    "randomgen/squares.pyx",
+    "randomgen/threefry.pyx",
+    "randomgen/tyche.pyx",
+    "randomgen/wrapper.pyx",
+    "randomgen/xoroshiro128.pyx",
+    "randomgen/xorshift1024.pyx",
+    "randomgen/xoshiro256.pyx",
+    "randomgen/xoshiro512.pyx",
+    "randomgen/_seed_sequence.pyx",
+    "randomgen/tests/_shims.pyx",
+]
+for pyx_file in pyx_files:
+    print(f"Cythonizing {pyx_file}")
+    cmd = base + [pyx_file]
+    print(" ".join(cmd))
+    subprocess.call(cmd)
